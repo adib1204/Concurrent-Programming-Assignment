45102cfa7c8784858f4b58e4dc3421587fe51940
Muhammad Akma Adhwa <muhdakma63@gmail.com>
2018-04-24 13:01:21 +0800

    change char array to String array

--- Traffic Signal Controller/src/Input.java
+++ Traffic Signal Controller/src/Input.java
@@ -1,34 +1,34 @@
 import java.io.*;
 import java.util.*;
+
 /**
- * Generate input randomly
- * Up to 6 input per direction
+ * Generate input randomly Up to 6 input per direction
  */
 public class Input implements Runnable {
-    private String fileName;
-    char direction;
+	private String fileName;
+	String direction;
 
-    public Input(String fileName, char direction) {
-        this.fileName=fileName;
-        this.direction=direction;
-    }
+	public Input(String fileName, String direction) {
+		this.fileName = fileName;
+		this.direction = direction;
+	}
 
-    public void run() {
-        Controller cl = new Controller();
-        Random rand = new Random();
-        int counter = rand.nextInt(5)+5;
-        cl.incrementCounter(counter);
-        try {
-            PrintWriter wr = new PrintWriter(new FileOutputStream(fileName));
-            
-            for (int i = 0; i < counter; i++) {
-                wr.println(direction);
-            }
-            
-            wr.close();
-        } catch (IOException e) {
-        }
-                
-    }
-    
+	public void run() {
+		Controller cl = new Controller();
+		Random rand = new Random();
+		int counter = rand.nextInt(5) + 5;
+		cl.incrementCounter(counter);
+		try {
+			PrintWriter wr = new PrintWriter(new FileOutputStream(fileName));
+
+			for (int i = 0; i < counter; i++) {
+				wr.println(direction);
+			}
+
+			wr.close();
+		} catch (IOException e) {
+		}
+
+	}
+
 }
--- Traffic Signal Controller/src/TestThread.java
+++ Traffic Signal Controller/src/TestThread.java
@@ -7,35 +7,35 @@ import java.util.concurrent.*;
  */
 public class TestThread {
     public static void main(String[] args) {
-        
+
         long initial = System.currentTimeMillis();
-        char[] direction = {'N','W','S','E'};
+        String[] direction = {"N","W","S","E"};
         String nameFile;
         Controller ct = new Controller();
-        
+
         System.out.println("0 L N R");
         System.out.println("0 L W R");
         System.out.println("0 L S R");
         System.out.println("0 L E R");
-        
+
         Light lit = new Light(initial);
         Thread td = new Thread(lit);
         td.start();
-        
+
         ExecutorService es = Executors.newCachedThreadPool();
-        
+
         for (int i = 0; i < direction.length; i++) {
             nameFile = "input "+direction[i]+".txt";
             es.execute(new Input(nameFile,direction[i]));
             es.execute(new Sensor(initial,(nameFile)));
         }
-        
+
         es.shutdown();
-        
+
         while(!es.isTerminated());
         lit.isEndTrue();
         while(td.isAlive());
         System.out.println("Program finish");
-        
+
     }
 }
