b1cc9bee30401074d91649b63d3b4c9b3a6df814
Siti Sholiha <sholiha.zakaria@gmail.com>
2018-05-04 17:01:24 +0800

    kklakhdjha

--- Traffic Signal Controller/src/TrainSensor.java
+++ Traffic Signal Controller/src/TrainSensor.java
@@ -1,3 +1,9 @@
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Random;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,4 +16,38 @@
  */
 public class TrainSensor {
     
+    private long initial;
+    private long stamp;
+    private String fileName;
+    Controller ctrl;
+    private boolean trainArriving;
+    
+    public TrainSensor(long timer, String fileName, Controller ctrl) {
+        this.initial = timer;
+        this.fileName = fileName;
+        this.ctrl = ctrl;
+    }
+    
+    synchronized public void run() {
+        String input;
+        Random rand = new Random();
+        int sleepTime = rand.nextInt(10000) + 20000;
+
+        try {
+            Thread.sleep(100); // Give time to write input file
+            BufferedReader rd = new BufferedReader(new FileReader(fileName));
+
+            while ((input = rd.readLine()) != null) {
+                Thread.sleep(sleepTime);
+                stamp = (System.currentTimeMillis() - initial) / 100 * 100;
+                System.out.println(stamp + " S " + input);
+                trainArriving = true;
+                if(trainArriving == true){
+                    wait();
+                }
+            }
+
+        } catch (IOException | InterruptedException e) {
+        }
+    }
 }
